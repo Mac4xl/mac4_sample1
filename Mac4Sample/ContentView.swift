c207a389f09012bb005fcfb5eefe8b7cf7f1ef3d
Mac4 <99777120+Mac4xl@users.noreply.github.com>
2023-03-26 20:02:50 +0900

    画面表示

--- Mac4Sample/ContentView.swift
+++ Mac4Sample/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  Mac4Sample
-//
-//  Created by yuksblog on 2023/01/01.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -16,50 +9,52 @@ struct ContentView: View {
         ZStack {
             SoccerBall(length: sensor.ballLength)
                 .position(sensor.currentBallPosition)
-        }
-        
-        HStack {
-            Text("(前後傾)°:")
-            Text(sensor.xStr2)
-            Text("(挙上下制)°:")
-            Text(sensor.yStr2)
-        }
-        
-        Button(action: {
-            self.sensor.isStarted ? self.sensor.stop() : self.sensor.start()
-        }) {
-            self.sensor.isStarted ? Text("STOP") : Text("START")
-        }
-        
-        Group {
-            Text("(前後傾)°: \(Int(sensor.thresholdAngle))")
-            Slider(value: $sensor.thresholdAngle, in: -180...30, step: 1)
-            Toggle("Sound", isOn: $sensor.soundEnabled)
-            Toggle("Stand", isOn: $sensor.Standing)
-            
-            Button(action: {
-                sensor.syncr()
-            }) {
-                Label("sync", systemImage: "personalhotspot.circle.fill")
-            }
-            
-            Button(action: {
-                sensor.share()
-            }) {
-                Label("share", systemImage: "square.and.arrow.up")
-            }
-            .onChange(of: scenePhase) { newScenePhase in
-                if newScenePhase == .background {
-                    self.sensor.stop()
+            VStack{
+                Group{
+                    HStack{
+                        Text("(前後傾)°:")
+                        Text(sensor.xStr2)
+                        Text("(挙上下制)°:")
+                        Text(sensor.yStr2)
+                        
+                    }
+                    Button(action: {
+                        self.sensor.isStarted ? self.sensor.stop() : self.sensor.start()
+                    }) {
+                        self.sensor.isStarted ? Text("STOP") : Text("START")
+                    }
+                    Text("(前後傾)°: \(Int(sensor.thresholdAngle))")
+                    Slider(value: $sensor.thresholdAngle, in: -180...30, step: 1)
+                    Toggle("Sound", isOn: $sensor.soundEnabled)
+                    Toggle("Stand", isOn: $sensor.Standing)
+                    
+                    Button(action: {
+                        sensor.syncr()
+                    }) {
+                        Label("sync", systemImage: "personalhotspot.circle.fill")
+                    }
+                    
+                    Button(action: {
+                        sensor.share()
+                    }) {
+                        Label("share", systemImage: "square.and.arrow.up")
+                    }
+                    Picker("", selection: $sensor.updateInterval) {
+                        Text("Hz:10").tag(0.1)
+                        Text("Hz:30").tag(0.0333)
+                        Text("Hz:60").tag(0.01667)
+                        Text("Hz:80").tag(0.0125)
+                        Text("Hz:100").tag(0.01)
+                    }
+                    
+                    .onChange(of: scenePhase) { newScenePhase in
+                        if newScenePhase == .background {
+                            self.sensor.stop()
+                        }
+                    }
                 }
             }
-            Picker("", selection: $sensor.updateInterval) {
-                Text("Hz:10").tag(0.1)
-                Text("Hz:30").tag(0.0333)
-                Text("Hz:60").tag(0.01667)
-                Text("Hz:80").tag(0.0125)
-                Text("Hz:100").tag(0.01)
-            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
         }
         .onChange(of: scenePhase) { newScenePhase in
             if newScenePhase == .background {
@@ -67,6 +62,7 @@ struct ContentView: View {
             }
         }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -74,5 +70,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-    
+
 
